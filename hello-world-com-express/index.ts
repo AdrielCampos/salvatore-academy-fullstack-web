6f09569ada29be6c569f206aff45250623a1704f
Adriel Campos <adrielcampos.dev@gmail.com>
2024-05-07 14:51:07 -0300

    feat: adding express

--- hello-world-com-express/index.ts
+++ hello-world-com-express/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello, world!");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({
+    message: "Hello World",
+  });
+});
+
+app.get("/oi", (req, res) => {
+  res.send({
+    message: "Olá Mundo",
+  });
+});
+
+app.listen(5050, () => {
+  console.log("Server is running on port 5050");
+});
